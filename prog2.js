2705b200e4f36eae41fecc659557c87807509ac1
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-05-25 16:33:06 +0530

    learned about array's and strings

---
+++ prog2.js
@@ -0,0 +1,24 @@
+let num=10;
+
+// let userinput=prompt("Enter the number: ");
+
+// while(userinput!=num)
+//     {
+//         let userinput=prompt("Enter the number: ");
+//         console.log("wrong :( please try again!!");
+//     }
+console.log("Congrats you entered the right number!!!!!!");
+
+let strtest="      Hello this is is is the message from the string"
+strtest=strtest.trim();
+
+strtest=strtest.toLowerCase();
+
+console.log(strtest)
+
+strtest=strtest.toUpperCase()
+console.log(strtest)
+
+strtest=strtest.replaceAll("is","here is")
+
+console.log(strtest)
