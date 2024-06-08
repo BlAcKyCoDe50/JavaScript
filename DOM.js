27e58d2ce558469dca0767f5f58e1c977b1843ab
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-06-08 12:32:53 +0530

    DOM manipulation

--- DOM.js
+++ DOM.js
@@ -1,2 +1,29 @@
+let div=document.querySelector("div")
+console.log(div)
+// let id=div.getAttribute("id")
+// console.log(id)
+// ***********inserting a new node**************
 
+let newbtn=document.createElement("button")
+// newbtn.innerText="Click me"
 
+// div.append(newbtn)
+// console.log(newbtn)
+
+// const newNode = document.createElement("span");
+
+//   // Get a reference to the parent node
+//   const parentDiv = document.getElementById("childElement").parentNode;
+
+//   // Begin test case [ 1 ] : Existing childElement (all works correctly)
+//   let sp2 = document.getElementById("childElement");
+//   parentDiv.insertBefore(newNode, sp2);
+//   // End test case [ 1 ]
+
+let newpara=document.createElement("h1");
+newpara.innerText="Hello this is the new inner text added by JS"
+
+let a=document.querySelector("body")
+a.prepend(newpara)
+
+console.log(newpara)
