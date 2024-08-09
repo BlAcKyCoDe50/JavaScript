ea0adc86ff71782fa5d5cdc64d6f87af13c287ad
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-08-09 19:37:54 +0530

    commit

--- javascript/prog2.js
+++ javascript/prog2.js
@@ -1,4 +1,4 @@
-let num=10;
+// let num=10;
 
 // let userinput=prompt("Enter the number: ");
 
@@ -7,18 +7,19 @@ let num=10;
 //         let userinput=prompt("Enter the number: ");
 //         console.log("wrong :( please try again!!");
 //     }
-console.log("Congrats you entered the right number!!!!!!");
+// console.log("Congrats you entered the right number!!!!!!");
 
-let strtest="      Hello this is is is the message from the string"
-strtest=strtest.trim();
+// let strtest="      Hello this is is is the message from the string"
+// strtest=strtest.trim();
 
-strtest=strtest.toLowerCase();
+// strtest=strtest.toLowerCase();
 
-console.log(strtest)
+// console.log(strtest)
 
-strtest=strtest.toUpperCase()
-console.log(strtest)
+// strtest=strtest.toUpperCase()
+// console.log(strtest)
 
-strtest=strtest.replaceAll("is","here is")
+// strtest=strtest.replaceAll("is","here is")
 
-console.log(strtest)
+// console.log(strtest)
+
