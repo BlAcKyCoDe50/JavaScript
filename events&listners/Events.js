d07718ceb71c2568721993e2bc3cc6cd8feeb8d9
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-08-16 13:22:48 +0530

    deleted unwanted files

--- events&listners/Events.js
+++
@@ -1,9 +0,0 @@
-const images = document.querySelector('#images')
-
-images.addEventListener('click',(e)=>{
-    // console.log(e.target.parentNode);  //for accessing the parent node
-    if(e.target.tagName === 'IMG'){
-    let removeit = e.target.parentNode
-    removeit.remove();
-    }
-},false)
